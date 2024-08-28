5ad4639403b725c9280cdd7097e2764efac0d86e
Diogo Gasparini <106988015+diogogaspdev@users.noreply.github.com>
2024-08-27 23:22:05 -0300

    cleanup and some setup

--- bot.py
+++ bot.py
@@ -68,7 +68,7 @@ It is recommended to use slash commands and therefore not use prefix commands.
 
 If you want to use prefix commands, make sure to also enable the intent below in the Discord developer portal.
 """
-# intents.message_content = True
+intents.message_content = True
 
 # Setup both of the loggers
 
@@ -166,6 +166,8 @@ class DiscordBot(commands.Bot):
                     self.logger.error(
                         f"Failed to load extension {extension}\n{exception}"
                     )
+
+        await self.tree.sync()
 
     @tasks.loop(minutes=1.0)
     async def status_task(self) -> None:
